160989a031042487cc6e35bbf9b19697cef0c9bc
Vivek <786vt786@gmail.com>
2023-02-08 20:24:20 +0530

    Men Do Feel internship Task

---
+++ app/src/main/java/com/example/menDoFeel/model/ExoPlayerItem.kt
@@ -0,0 +1,8 @@
+package com.example.menDoFeel.model
+
+import com.google.android.exoplayer2.ExoPlayer
+
+class ExoPlayerItem(
+    var exoPlayer: ExoPlayer,
+    var position: Int
+)
---
+++ app/src/main/java/com/example/menDoFeel/model/Video.kt
@@ -0,0 +1,6 @@
+package com.example.menDoFeel.model
+
+class Video(
+    var title: String,
+    var url: String
+)
